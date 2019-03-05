4f45b4e0538152dc22a1b37c303138f4a9600137
Campbell Woodside <campbell.woodside@ocsbstudent.ca>
2019-03-05 14:59:15 -0500

    Animating Images

--- AnimatingImagesCampbellW/main.lua
+++ AnimatingImagesCampbellW/main.lua
@@ -1,5 +1,5 @@
 -- title: animating images
-
+--
 -- set background
 local background = display.newImageRect("Images/ocean2.jpg", 2048, 1536)
 
@@ -36,7 +36,7 @@ Runtime:addEventListener("enterFrame", MoveShip)
 -- BOAT
 
 scrollSpeed3 = -6
-
+scrollSpeed6 = 2
 local boat = display.newImageRect("Images/boat.png", 200, 200)
 boat.x = 900
 boat.y = display.contentHeight/1.5
@@ -52,6 +52,7 @@ boat.alpha = 0
 local function MoveBoat(event)
 	-- add scroll speed to x value
 	boat.x = boat.x + scrollSpeed3
+	boat.y = boat.y + scrollSpeed6
 	-- fade in
 	boat.alpha = boat. alpha + 0.01
 	-- grow in size
@@ -68,9 +69,31 @@ Runtime:addEventListener("enterFrame", MoveBoat)
 ----------------------------------------------------------------------------------------------------------
 
 scrollSpeed4 = -10
+scrollSpeed5 = 10
 
 local skidoo = display.newImageRect("Images/skidoo.png", 200, 200)
-skidoo.x = 500
-skidoo.y = display.contentHeight/1.5
+skidoo.x = 900
+skidoo.y = display.contentHeight/5
 
+-- MoveSkidoo
+local function MoveSkidoo(event)
+	-- add scrollSpeed4 +scrollSpeed5
+	skidoo.x = skidoo.x + scrollSpeed4
+	skidoo.y = skidoo.y + scrollSpeed5
 
+	-- grow so it looks like its getting closer
+	skidoo.xScale = skidoo.xScale + 0.01
+	skidoo.yScale = skidoo.yScale + 0.01
+
+end
+
+-- MoveSkidoo will be called over and over again
+Runtime:addEventListener("enterFrame", MoveSkidoo)
+
+---------------------------------------------------------------------------------------------------------------
+
+local myTextX = display.contentWidth* 2/5
+local myTextY = display.contentHeight/1.5
+
+local myText = display.newText("By: Campbell Woodside", myTextX, myTextY, Arial, 50)
+myText:setFillColor(1, 0.2, 0.2)
