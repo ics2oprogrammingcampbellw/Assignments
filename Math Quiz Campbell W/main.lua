a97e1c67d66c41cc488a96ef7f30972024d6cc35
Campbell Woodside <campbell.woodside@ocsbstudent.ca>
2019-04-02 11:11:46 -0400

    Update main.lua

--- Math Quiz Campbell W/main.lua
+++ Math Quiz Campbell W/main.lua
@@ -1,7 +1,28 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
+--Title: Numeric Text Fields
+--Name:Campbell W
+-- Course: ICS 20/3C
+-- This program asks math questions and corrects them until they get five questions correct.
+
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- sets the background colour
+display.setDefault("background", 124/255, 249/255, 199/255)
+
+-----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+
+-- create local variables
+local questionObject
+local correctObject
+local numericField
+local randomNumber1
+local randomNumber2
+local userAnswer
+local correctAnswer
+
+-----------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
